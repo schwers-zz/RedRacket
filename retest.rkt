236b1ad9423249ab4687947bf4b356a62ec07eb0
Ryan Schwers <schwers.r@gmail.com>
2011-02-16 20:11:26 -0500

    Updated retest to run tests. Supports basic output to file

--- retest.rkt
+++ retest.rkt
@@ -1,91 +1,94 @@
 ;; Test Suite and test DFA's
-(module redtest racket
+(module test racket
    (require "red.rkt"
-            mzlib/integer-set)
+            mzlib/integer-set
+            racket/unsafe/ops)
    ;; Again, just to make testing from REPL easier....
    (provide
     (combine-out (all-defined-out)
                  (all-from-out "red.rkt")
                  (all-from-out mzlib/integer-set)))
 
+   ;; Build a set of functions corresponding to an RE->DFA
+   ;; use this like (define f1 (benchmark dfa))
+   ;;               (f1 "someinput")
    (define-syntax benchmark
      (syntax-rules ()
        [(_ dfa)
         (eval-syntax (dfa-expand dfa))]))
 
-  ;; Test DFA's From deriv.rkt, making sure we can build everything
-  (define t1 (build-test-dfa null))
-  (define t2 (build-test-dfa `(#\a)))
-  (define t3 (build-test-dfa `(#\a #\b)))
-  (define t4 (build-test-dfa `((repetition 0 +inf.0 #\a)
-                               (repetition 0 +inf.0 (concatenation #\a #\b)))))
-  (define t5 (build-test-dfa `((concatenation (repetition 0 +inf.0 (union #\0 #\1)) #\1))))
-  (define t6 (build-test-dfa `((repetition 0 +inf.0 (repetition 0 +inf.0 #\a))
-                               (repetition 0 +inf.0 (concatenation #\b (repetition 1 +inf.0 #\b))))))
-  (define t7 (build-test-dfa `((concatenation (repetition 0 +inf.0 #\a) (repetition 0 +inf.0 #\b)
-                                              (repetition 0 +inf.0 #\c) (repetition 0 +inf.0 #\d)
-                                                (repetition 0 +inf.0 #\e)))))
-  (define t8
-    (build-test-dfa `((concatenation (repetition 0 +inf.0 (union #\a #\b)) #\a (union #\a #\b)
-                                     (union #\a #\b) (union #\a #\b) (union #\a #\b)))))
-  (define t9 (build-test-dfa `((concatenation "/*"
-                                              (complement (concatenation (intersection) "*/" (intersection)))
-                                                "*/"))))
-  (define t11 (build-test-dfa `((complement "1"))))
-  (define t12 (build-test-dfa `((concatenation (intersection (concatenation (repetition 0 +inf.0 "a") "b")
-                                                             (concatenation "a" (repetition 0 +inf.0 "b")))
-                                               "ab"))))
-  (define x (build-test-dfa `((union " " "\n" ",")
-                              (concatenation (repetition 0 1 "-") (repetition 1 +inf.0 (char-range "0" "9")))
-                              (concatenation "-" (repetition 1 +inf.0 "-"))
-                              "["
-                              "]")))
-  (define y (build-test-dfa
-             `((repetition 1 +inf.0
-                           (union (concatenation "|" (repetition 0 +inf.0 (char-complement "|")) "|")
-                                  (concatenation "|" (repetition 0 +inf.0 (char-complement "|"))))))))
-  (define t13 (build-test-dfa `((intersection (concatenation (intersection) "111" (intersection))
-                                              (complement (union (concatenation (intersection) "01")
-                                                                 (repetition 1 +inf.0 "1")))))))
-  (define t14 (build-test-dfa `((complement "1"))))
+   ;; Around for turning a string into an equivalent char
+   ;; list, strictly for the 'old' version of dfa-expand
+   (define (explode str)
+     (let loop ((i 0) (n (unsafe-string-length str)) (acc '()))
+       (cond [(unsafe-fx= i n) (reverse acc)]
+             [else (loop (unsafe-fx+ 1 i)
+                         n
+                         (cons (unsafe-string-ref str i) acc))])))
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;; Tests
-  ;; This should eval to true
-  ;; (eval (cons (syntax->datum (dfa-expand t4)) (cons (quote (cons #\a (cons #\a (cons #\a (cons #\a empty))))) empty)))
 
-  ;; Some tests from re.rkt
-  (test-block ((c (make-cache))
-               (a (char->integer #\a))
-               (b (char->integer #\b))
-               (r1 (->re #\a c))
-               (r2 (->re `(repetition 0 +inf.0 #\a) c))
-               (r3 (->re `(repetition 0 +inf.0 ,r2) c))
-               (r4 (->re `(concatenation #\a ,r2) c))
-               (r5 (->re `(repetition 0 +inf.0 ,r4) c))
-               (r6 (->re `(union ,r5 #\a) c))
-               (r7 (->re `(concatenation ,r2 ,r2) c))
-               (r8 (->re `(complement ,r4) c))
-               (r9 (->re `(intersection ,r2 ,r4) c)))
-              ((deriveR e a c) z)
-              ((deriveR z a c) z)
-              ((deriveR r1 b c) z)
-              ((deriveR r1 a c) e)
-              ((deriveR r2 a c) r2)
-              ((deriveR r2 b c) z)
-              ((deriveR r3 a c) r2)
-              ((deriveR r3 b c) z)
-              ((deriveR r4 a c) r2)
-              ((deriveR r4 b c) z)
-              ((deriveR r5 a c) (->re `(concatenation ,r2 ,r5) c))
-              ((deriveR r5 b c) z)
-              ((deriveR r6 a c) (->re `(union (concatenation ,r2 ,r5) "") c))
-              ((deriveR r6 b c) z)
-              ((deriveR r7 a c) (->re `(union (concatenation ,r2 ,r2) ,r2) c))
-              ((deriveR r7 b c) z)
-              ((deriveR r8 a c) (->re `(complement, r2) c))
-              ((deriveR r8 b c) (->re `(complement ,z) c))
-                ((deriveR r9 a c) r2)
-                ((deriveR r9 b c) z)
-                ((deriveR (->re `(repetition 1 2 "ab") c) a c)
-                 (->re `(concatenation "b" (repetition 0 1 "ab")) c))))
+   ;; Tests -- DFA + regexp + input, and an associated test
+
+
+   ;; A DFA-MATCH is an output of dfa-expand
+   ;; with contract : (listof char) -> boolean
+
+   ;; : DFA-MATCH RE (listof char) regexp-string string string Nat -> Unit
+   (define (compare-speed dfa-match dfa-in re re-in description size)
+     (printf "Now testing: ~a ~n" description)
+     (printf "Input size approx: ~a ~n" size)
+     (printf "Built in re matcher: ~n")
+     (time (regexp-match? re re-in))
+     (printf "DFA-Match: ~n")
+     (time (dfa-match dfa-in))
+     (printf "end test~n~n"))
+
+
+   ;; .*schwers.r@gmail.com.*
+   (define *email*
+     (benchmark
+      (build-test-dfa
+       '((concatenation (repetition 0 +inf.0 (char-range "0" "z"))
+                        #\s #\c #\h #\w #\e #\r #\s #\. #\r
+                        #\@ #\g #\m #\a #\i #\l #\. #\c #\o #\m
+                        (repetition 0 +inf.0 (char-range "0" "z")))))))
+
+
+   (define s1 (make-string 100000 #\a))
+   (define s2 (make-string 1000000 #\a))
+   (define email "schwers.r@gmail.com")
+   (define str1 (string-append s1 email s1))
+   (define str2 (string-append s2 email s2))
+   (define str1-list (explode str1))
+   (define str2-list (explode str2))
+
+   (define (t1)
+     (compare-speed *email* str1-list  "schwers.r@gmail.com" str1
+                   "*schwers.r@gmail.com*" 20000))
+
+
+   (define (t2)
+     (compare-speed *email* str2-list "schwers.r@gmail.com" str2
+                    "*schwers.r@gmail.com*" 2000000))
+
+   ;; ^a*$
+   (define a*-only (benchmark (build-test-dfa '((repetition 0 +inf.0 #\a)))))
+
+   (define a*-only-list (explode s2))
+
+   (define (t3)
+     (compare-speed a*-only a*-only-list "^a*$" str2
+                     "only a* -- ^a*$" 2000000))
+
+   ;; (listof tests)
+   (define all-tests (list t1 t2 t3))
+
+   (define (run-tests)
+     (map (lambda (x) (x)) all-tests))
+
+   (define (run-tests-log-to name)
+     (with-output-to-file (string-append "testdata/"name)
+       run-tests))
+
+   (run-tests-log-to "testlog1.txt")
+)
