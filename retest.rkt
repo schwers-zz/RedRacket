4afd80c49e8b892257c4756da5bf358cee92d4d6
Ryan Schwers <schwers.r@gmail.com>
2011-03-10 16:41:08 -0500

    Added tests

--- retest.rkt
+++ retest.rkt
@@ -1,91 +1,91 @@
 ;; Test Suite and test DFA's
 (module retest racket
-   (require "red.rkt"
-            racket/unsafe/ops
-            (for-syntax "red.rkt"
-                        racket
-                        racket/unsafe/ops))
-   ;; Again, just to make testing from REPL easier....
-   (provide
-    (combine-out (all-defined-out)))
+  (require "red.rkt"
+           racket/unsafe/ops
+           (for-syntax "red.rkt"
+                       racket
+                       racket/unsafe/ops))
+        ;; Again, just to make testing from REPL easier....
+  (provide
+   (combine-out (all-defined-out)))
 
-   ;; control parameters for tests
-   (define regexp-tests? #t)
-   (define dfa-tests? #t)
+  ;; control parameters for tests
+  (define regexp-tests? #t)
+  (define dfa-tests? #t)
 
-   (define all-tests '())
+  (define all-tests '())
 
-   ;; : symbol -> boolean ; returns true if the test should be run
-   (define (run-test? which)
-     (or (and (equal? which 'regexp) regexp-tests?)
-         (and (equal? which 'dfa) dfa-tests?)))
+  ;; : symbol -> boolean ; returns true if the test should be run
+  (define (run-test? which)
+    (or (and (equal? which 'regexp) regexp-tests?)
+        (and (equal? which 'dfa) dfa-tests?)))
 
-   ;; : (string -> boolean) string string symbol -> Unit
-   ;; takes a function of a string, which returns boolean based on a match
-   ;; runs the test according to the controls, and prints results
-   (define (test-match matcher input test which)
-     (if (run-test? which)
-         (begin
-           (set! all-tests
-                 (append all-tests
-                         (list (lambda ()
-                                 (printf "TESTING: ~s~n" test)
-                                 (printf "Input size: ~a~n" (string-length input))
-                                 (printf "Matcher : ~a~n" which)
-                                 (time (matcher input))
-                                 (printf "~n")))))
-           (printf "ADDED ~a:~a to tests~n" which test))
-         (printf "~a:~a was not added~n" which test)))
+  ;; : (string -> boolean) string string symbol -> Unit
+  ;; takes a function of a string, which returns boolean based on a match
+  ;; runs the test according to the controls, and prints results
+  (define (test-match matcher input test which)
+    (if (run-test? which)
+        (begin
+          (set! all-tests
+                (append all-tests
+                        (list (lambda ()
+                                (printf "TESTING: ~s~n" test)
+                                (printf "Input size: ~a~n" (string-length input))
+                                (printf "Matcher : ~a~n" which)
+                                (time (matcher input))
+                                (printf "~n")))))
+          (printf "ADDED ~a:~a to tests~n" which test))
+        (printf "~a:~a was not added~n" which test)))
 
-   (define (test-dfa dfa input test) (test-match dfa input test 'dfa))
+  (define (test-dfa dfa input test) (test-match dfa input test 'dfa))
 
-   (define (test-reg regexp input test)
-     (test-match (lambda (str) (regexp-match? regexp str))
-                 input test 'regexp))
-
-
-   ;; : string number -> string
-   ;; helper function to build large strings
-   (define (build-by-twos str pow)
-     (if (<= pow 0) str
-         (build-by-twos (string-append str str) (- pow 1))))
-
-   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-   ;; .*schwers.r@gmail.com.*
-   (define-for-syntax *email*-stx
-     (dfa-expand
-      (build-test-dfa
-       '((concatenation (repetition 0 +inf.0 (char-complement))
-                        #\s #\c #\h #\w #\e #\r #\s #\. #\r
-                        #\@ #\g #\m #\a #\i #\l #\. #\c #\o #\m
-                        (repetition 0 +inf.0 (char-complement)))))))
-
-   (define-syntax (bench-*email* stx)
-     (syntax-case stx ()
-       [(_) *email*-stx]))
-
-   (define *email* (bench-*email*))
-
-   (define s1 (build-by-twos "a" 20))
-   (define s2 (build-by-twos "a" 26))
-   (define email "schwers.r@gmail.com")
-   (define str1 (string-append s1 email s1))
-   (define str2 (string-append s2 email s2))
+  (define (test-reg regexp input test)
+    (test-match (lambda (str) (regexp-match? regexp str))
+                input test 'regexp))
 
 
-   (define *email*-desc ".*schwers.r@gmail.com.*")
-   (test-dfa *email* str1 *email*-desc)
-   (test-dfa *email* str2 *email*-desc)
-
-   (define *email*-regex  #rx"schwers.r@gmail.com")
-   (test-reg *email*-regex str1 *email*-desc)
-   (test-reg *email*-regex str2 *email*-desc)
+  ;; : string number -> string
+  ;; helper function to build large strings
+  (define (build-by-twos str pow)
+    (if (<= pow 0) str
+        (build-by-twos (string-append str str) (- pow 1))))
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-   ;; ^a*$
-   (define-for-syntax a*-only-stx
-     (dfa-expand
-      (build-test-dfa '((repetition 0 +inf.0 #\a)))))
+  ;; .*schwers.r@gmail.com.*
+  (define-for-syntax *email*-stx
+    (dfa-expand
+     (build-test-dfa
+      '((concatenation (repetition 0 +inf.0 (char-complement))
+                       #\s #\c #\h #\w #\e #\r #\s #\. #\r
+                       #\@ #\g #\m #\a #\i #\l #\. #\c #\o #\m
+                       (repetition 0 +inf.0 (char-complement)))))))
+
+  (define-syntax (bench-*email* stx)
+    (syntax-case stx ()
+      [(_) *email*-stx]))
+
+  (define *email* (bench-*email*))
+
+  (define s1 (build-by-twos "a" 20))
+  (define s2 (build-by-twos "a" 26))
+  (define email "schwers.r@gmail.com")
+  (define str1 (string-append s1 email s1))
+  (define str2 (string-append s2 email s2))
+
+
+  (define *email*-desc ".*schwers.r@gmail.com.*")
+  (test-dfa *email* str1 *email*-desc)
+  (test-dfa *email* str2 *email*-desc)
+
+  (define *email*-regex  #rx"schwers.r@gmail.com")
+  (test-reg *email*-regex str1 *email*-desc)
+  (test-reg *email*-regex str2 *email*-desc)
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; ^a*$
+  (define-for-syntax a*-only-stx
+    (dfa-expand
+     (build-test-dfa '((repetition 0 +inf.0 #\a)))))
 
    (define-syntax (bench-a*-only stx)
      (syntax-case stx ()
@@ -122,13 +122,13 @@
      (dfa-expand
       (build-test-dfa
        '((concatenation (repetition 1 +inf.0 (union (char-range #\a #\z)
-                                                     (char-range #\0 #\9)
-                                                     #\. #\_ #\% #\+ #\-))
-                         #\@
-                         (repetition 1 +inf.0 (union (char-range #\a #\z)
+                                                    (char-range #\0 #\9)
+                                                    #\. #\_ #\% #\+ #\-))
+                        #\@
+                        (repetition 1 +inf.0 (union (char-range #\a #\z)
                                                      (char-range #\0 #\9)
                                                      #\. #\-))
-                         (repetition 2 4 (char-range #\a #\z)))))))
+                        (repetition 2 4 (char-range #\a #\z)))))))
 
    (define-syntax (bench-valid-email stx)
      (syntax-case stx ()
@@ -157,6 +157,31 @@
    (test-reg email-regex (string-append em4 ".uk") email-desc)
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;; web address
+
+   (define-for-syntax *web-stx*
+     (dfa-expand
+      (build-test-dfa
+       '(#\w #\w #\w #\.
+         (concatenation  (repetition 1 +inf.0 (char-range #\a #\z))
+                         #\.
+                         (repetition 2 4 (char-range #\a #\z)))))))
+
+   (define-syntax (bench-web stx)
+     (syntax-case stx ()
+       [(_) *web-stx*]))
+
+   (define web (bench-web))
+
+   (define webfiller (string-append "www." (build-by-twos "lambdafxxfxoiasdf" 10) ".com"))
+
+   (test-dfa web webfiller "website url, should pass")
+
+   (define web-regex #rx"^www\\.[a-z]+\\.([a-z][a-z]|[a-z][a-z]|[a-z][a-z][a-z][a-z])$")
+
+   (test-reg web-regex webfiller "website url, should pass")
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
    ;; Running tests, with optional file output
    (define (run-tests)
      (map (lambda (x) (x)) all-tests)
@@ -165,8 +190,8 @@
    (define (log-to name)
      (with-output-to-file name
        (lambda ()
-         (printf "Testdata from retest.rkt~n~n")
-         (run-tests)))
+         (printf "Performance test data~n~n")
+         (run-tests))))
 
-   ;;(log-to "testdata.txt")
+   (log-to "data2.txt")
 )
