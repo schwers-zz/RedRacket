5d82a18998f34e534e6c53929be488c3446e0f04
Ryan Schwers <schwers.r@gmail.com>
2011-04-12 17:06:48 -0400

    Add a user interface! It appears to work. More regexp-syntax support coming soon

---
+++ redracket.rkt
@@ -0,0 +1,34 @@
+
+(module redracket racket
+  (require "red2.rkt"
+           "front.rkt"
+           racket/unsafe/ops
+           (for-syntax "red2.rkt"
+                       "front.rkt"
+                       racket
+                       racket/unsafe/ops)
+           (for-meta 2 "red2.rkt"
+                       "front.rkt"
+                       racket
+                       racket/unsafe/ops))
+
+
+  ;; The macro for user level
+  (define-syntax (dfa-match? stx)
+    (syntax-case stx ()
+      [(_ regexp string)
+       (with-syntax
+        ([dfa-matcher (dfa-expand (build-test-dfa (->redstring (syntax->datum #'regexp))))])
+         #'(dfa-matcher string))]))
+
+
+  ;; Example tests
+  (equal? (dfa-match? "a+" "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") #t)
+  (equal? (dfa-match? "c(a|d)*r" "caadadadaddaadadadadadadddddaaaaar") #t)
+  (equal? (dfa-match? "schwers.r@gmail.com" "aosihdfasdfschwers.r@gmail.comaskdfas") #t)
+  (equal? (dfa-match? "^schwers.r@gmail.com$" "schwers.r@gmail.com") #t)
+  (equal? (dfa-match? "(node\\.js|ruby)" "lashdfasdfnode\\.jsihasdfiasd") #t)
+  (equal? (dfa-match? "(red|racket)(red|racket)" "redracket") #t)
+
+)
+
