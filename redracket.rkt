923aa27d30e2a535fccd03a3a3893ecbf9a9896c
Ryan Schwers <schwers.r@gmail.com>
2011-04-13 13:51:44 -0400

    Add utility function for inspecting output of the front-end

--- redracket.rkt
+++ redracket.rkt
@@ -1,6 +1,6 @@
 
 (module redracket racket
-  (require "red2.rkt"
+  (require "red.rkt"
            "front.rkt"
            racket/unsafe/ops
            (for-syntax "red2.rkt"
@@ -14,6 +14,10 @@
 
   (provide dfa-match?)
 
+  ;; For testing purposes -- numbers seem odd right now
+  (define (inspect string)
+    (syntax->datum (dfa-expand (build-test-dfa (->redstring string)))))
+
 
   ;; The macro for user level
   (define-syntax (dfa-match? stx)
@@ -24,13 +28,14 @@
          #'(dfa-matcher string))]))
 
 
-  ;; Example tests
+  #|
+  Example tests
   (equal? (dfa-match? "a+" "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") #t)
   (equal? (dfa-match? "c(a|d)*r" "caadadadaddaadadadadadadddddaaaaar") #t)
   (equal? (dfa-match? "schwers.r@gmail.com" "aosihdfasdfschwers.r@gmail.comaskdfas") #t)
   (equal? (dfa-match? "^schwers\\.r@gmail\\.com$" "schwers.r@gmail.com") #t)
   (equal? (dfa-match? "(node\\.js|ruby)" "lashdfasdfnode\\.jsihasdfiasd") #t)
   (equal? (dfa-match? "(red|racket)(red|racket)" "redracket") #t)
-
+  |#
 )
 
